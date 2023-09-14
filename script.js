b7eb13a0b99386e72a5586ee9688c2a24f398b0e
nicososa01 <nicolassosacarsin@gmail.com>
2023-09-13 21:22:35 -0300

    Solucion subida
    Trabajo en clase

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
+//Se crea una variable para el boton tomando el id "btn"
 var boton = document.getElementById("btn");
+//Se crea una variable para el box(caja) con el id "caja"
 var box = document.getElementById("caja");
 
+//Es una funcion para cambiar de color las letras
 function CambiarColor() {
     var letters = "0123456789ABCDEF";
     var color = "#";
